613cc8ad1ed9b293397b1fe5a1f38606c1f3453d
Brandon Garcia <grandonleebarcia@gmail.com>
2019-03-15 18:41:52 -0400

    initial commit:

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
-  
+
   //When button is clicked, run function
-  $("#getRandomQuote").on("click",function() {
+  $("#getRandomQuote").on("click",function(e) {
 
     //Retrieve JSON and pass data as parameter
-   $.getJSON("https://talaikis.com/api/quotes/random/", function(data) {
 
-      //Output json data to respective classes on page
-      $(".quote").html(data.quote);
-      $(".author").html(data.author);
+      e.preventDefault();
+      $.ajax( {
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        dataType: "json",
+        success: function(data) {
+          
+          $(".quote").html(data[0].content);
+          $(".author").html(data[0].title);
+        },
+        cache: false
+      });
+
     });
   });
-});
