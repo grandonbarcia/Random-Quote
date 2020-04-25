0a3a670a504b165e6e51be81809054c8c11e07bf
Brandon Garcia <grandonleebarcia@gmail.com>
2020-04-25 10:32:38 -0400

    fixed merging conflicts

--- js/index.js
+++ js/index.js
@@ -7,22 +7,19 @@ $(document).ready(function() {
 
       e.preventDefault();
       $.ajax( {
-<<<<<<< HEAD
         url: 'https://favqs.com/api/qotd',
         dataType: "json",
         success: function(data) {
 
         $(".quote").html(data.quote.body);
         $(".author").html(data.quote.author);
-        
-=======
+
         url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
         dataType: "json",
         success: function(data) {
-          
+
           $(".quote").html(data[0].content);
           $(".author").html(data[0].title);
->>>>>>> 613cc8ad1ed9b293397b1fe5a1f38606c1f3453d
         },
         cache: false
       });
