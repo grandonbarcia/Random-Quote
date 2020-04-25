7e43b93a96116b6ff26fd16a9424785f7b108341
Brandon Garcia <grandonleebarcia@gmail.com>
2020-04-25 10:18:55 -0400

    replaced api url

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-  
+
   //When button is clicked, run function
-  $("#getRandomQuote").on("click",function() {
+  $("#getRandomQuote").on("click",function(e) {
 
     //Retrieve JSON and pass data as parameter
-   $.getJSON("https://talaikis.com/api/quotes/random/", function(data) {
 
-      //Output json data to respective classes on page
-      $(".quote").html(data.quote);
-      $(".author").html(data.author);
+      e.preventDefault();
+      $.ajax( {
+        url: 'https://favqs.com/api/qotd',
+        dataType: "json",
+        success: function(data) {
+
+        $(".quote").html(data.quote.body);
+        $(".author").html(data.quote.author);
+        
+        },
+        cache: false
+      });
+
     });
   });
-});
